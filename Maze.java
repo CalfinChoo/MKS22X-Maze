e212179c0e9afacd28004f7895239eda882f86e6
Calvin Chu <cchu00@stuy.edu>
2019-02-27 11:18:37 -0500

    classwork

--- Maze.java
+++ Maze.java
@@ -3,12 +3,27 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Maze{
   public static String maze = "";
+  public static char[][] charMaze;
   public Maze() {
     try {
       File text = new File("Mazel.txt");
       Scanner in = new Scanner(text);
+      int rows = 0;
+      int cols = 0;
       while(in.hasNextLine()) {
-        maze += in.nextLine() + "\n";
+        String s = in.nextLine();
+        maze += s + "\n";
+        rows++; cols++;
+      }
+      charMaze = new char[rows][cols];
+      Scanner inC = new Scanner(text);
+      int countRow = 0;
+      while(inC.hasNextLine()) {
+        String s = inC.nextLine();
+        for (int i = 0; i < s.length(); i++) {
+          charMaze[countRow][i] = s.charAt(i);
+        }
+        if (s.length() > 0) countRow++;
       }
     } catch(FileNotFoundException e) {
       e.printStackTrace();
@@ -16,6 +31,14 @@ public class Maze{
   }
   public static void main(String[] args) {
     Maze m = new Maze();
-    System.out.print(m.maze);
+    //System.out.print(m.maze);
+    String s = "";
+    for (char[] x : charMaze) {
+      for (char y : x) {
+        s += y;
+      }
+      s += "\n";
+    }
+    System.out.print(s);
   }
 }
